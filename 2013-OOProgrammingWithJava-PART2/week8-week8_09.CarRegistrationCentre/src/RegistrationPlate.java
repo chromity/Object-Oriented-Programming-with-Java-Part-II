d1ffede3aedb6f1d905bb8be9ea938eab3857668
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-07-07 23:42:54 +0800

    Partially completed exercise 09
    I will continue this tomorrow.

---
+++ 2013-OOProgrammingWithJava-PART2/week8-week8_09.CarRegistrationCentre/src/RegistrationPlate.java
@@ -0,0 +1,45 @@
+
+public class RegistrationPlate {
+    // don't change the code which is already given to you
+
+    // ATTENTION: the object variable types are final, meaning that their value cannot be changed!
+    private final String regCode;
+    private final String country;
+
+    public RegistrationPlate(String country, String regCode) {
+        this.regCode = regCode;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return country + " " + regCode;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RegistrationPlate other = (RegistrationPlate) obj;
+        if ((this.regCode == null) ? (other.regCode != null) : !this.regCode.equals(other.regCode)) {
+            return false;
+        }
+        if ((this.country == null) ? (other.country != null) : !this.country.equals(other.country)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        if (this.regCode == null) {
+            return -1;
+        }
+        
+        return this.regCode.hashCode();
+    }
+}
